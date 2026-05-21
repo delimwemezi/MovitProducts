86c0ca176048f611738014c174038f64b4d86fb1
delimwemezi <delimwemezi@gmail.com>
2026-05-21 12:21:12 +0300

    Update 2026_04_10_125042_create_products_table.php

--- database/migrations/2026_04_10_125042_create_products_table.php
+++ database/migrations/2026_04_10_125042_create_products_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->text('description')->nullable();
-            $table->decimal('price', 10, 2);
+            $table->decimal('CartonPrice', 10, 2);
+            $table->decimal('PiecePrice', 10, 2);
             $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->string('image')->nullable();
             $table->timestamps();
