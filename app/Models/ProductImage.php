9d7350a039a3d8d3b2858fbd3854a1290a2b62da
delimwemezi <delimwemezi@gmail.com>
2026-07-06 12:02:52 +0300

    Create ProductImage model for image storage

---
+++ app/Models/ProductImage.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductImage extends Model
+{
+    protected $fillable = [
+        'product_id',
+        'image_data',
+        'mime_type',
+        'original_filename',
+        'file_size',
+        'is_cloudinary',
+        'cloudinary_url',
+    ];
+
+    /**
+     * Relationship: Image belongs to a Product
+     */
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    /**
+     * Get the data URL for displaying in img src
+     * Returns base64 encoded image data
+     */
+    public function getDataUrlAttribute(): string
+    {
+        $base64 = base64_encode($this->image_data);
+        return "data:{$this->mime_type};base64,{$base64}";
+    }
+}
